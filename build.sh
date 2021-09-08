163b428bab17f5294b2c81a35fa685182a35cb8a
adisonInc <mental76outlaw@protonmail.com>
2021-09-08 18:08:42 +0000

    upload

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #/bin/bash
-sudo pacman -S feh zsh xcompmgr xorg xorg-server curl ttf-fira-code
-cd $HOME/artixRice
+echo "provide your username"
+read user
+sudo pacman -S feh zsh xcompmgr xorg-init make xorg-server curl ttf-fira-code
+cp /home/$user/aritxRice/.xinitrc /home/$user
+cp -r  /home/$user/aritxRice/.themes /home/$user
+cp /home/$user/aritxRice/.arch.png /home/$user
+cp /home/$user/aritxRice/.zshrc /home/$user
+cd home/$user/artixRice
 cd dwm
 sudo make clean install
 cd ..
@@ -11,14 +17,11 @@ cd slstatus
 sudo make clean install
 cd ..
 cd st 
-sudo make clean isntall
-cd cd ..
-cp .xinitrc $HOME
-cp .arch.png $HOME
-cp -r .themes $HOME
+sudo make clean install
+cd /home/$user
 gsettings set org.gnome.desktop.interface gtk-theme "Nordic"
 gsettings set org.gnome.desktop.wm.preferences theme "Nordic"
-feh --bg-scale .arch.png
+feh --bg-scale /home/$user/.arch.png
 xcompmgr -c -r 0
 
 
